b53fa88459bd7967caba5c966b2b7e62556c98fd
Daresay-edu <15302130568@163.com>
2019-04-09 22:26:15 +0800

    use js to check engname and password whether blank or not

--- login.php
+++ login.php
@@ -8,19 +8,31 @@
 <title>Daresay Education</title> 
 <link href="css/login.css" type="text/css" rel="stylesheet"> 
 </head> 
+<script>
+function check_submit(){
+	var engname = document.getElementById("engname").value;
+	var password = document.getElementById("password").value;
+	if (engname.length == 0 || password.length == 0) {
+		alert("英文名和密码不能为空");
+		return false;
+	} else {
+		return true;
+	}
+}
+</script>
 <body> 
 
 <div class="login">
     <div class="message">Daresay Education</div>
     <div id="darkbannerwrap"></div>
     
-    <form action="login.php?action=check" method="post">
+    <form name="lg" id="lg" action="login.php?action=check" method="post" onsubmit="return check_submit();">
 		<input name="action" value="login" type="hidden">
-		<input name="engname" id="engname" placeholder="用户名" required="" type="text">
+		<input name="engname" id="engname" placeholder="用户名" required="required" type="text">
 		<hr class="hr15">
-		<input name="password" id="password" placeholder="密码" required="" type="password">
+		<input name="password" id="password" placeholder="密码" required="required" type="password">
 		<hr class="hr15">
-		<input type="submit" class= 'submit' name="add" value="登录" onclick="return check_login();"/>
+		<input type="submit" class= 'submit' name="add" value="登录"/>
 		<hr class="hr20">
 		<!-- 帮助 <a onClick="alert('请联系管理员')">忘记密码</a> -->
 	</form>
